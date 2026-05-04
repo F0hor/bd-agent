bd4c0d9bec4f6bff2573feab74ab9ca67d6aa7de
F0hor <f.hor@proton.me>
2026-05-04 19:44:45 +0200

    basic agent response

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from bd-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+if api_key == None or api_key == "":
+    raise Exception('No API key')
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', 
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+print(response.text)
